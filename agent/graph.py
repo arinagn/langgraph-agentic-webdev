cc8c981ecab7a3b6ec3ad8bf1fcf219e71bc2737
arinagn <arina.ghanizad@gmail.com>
2025-11-15 13:41:29 +0300

    Adding conditional edge from code to END when status=DONE.

--- agent/graph.py
+++ agent/graph.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.globals import set_verbose, set_debug
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, END
 from langchain.agents import create_agent
 from prompts import *
 from states import *
@@ -44,7 +44,7 @@ def architect_agent(state: dict) -> dict:
 
 def coder_agent(state: dict) -> dict:
     """Generates CoderState for each filepath in TaskPlan.
-    
+
     Makes calls to LangGraph tools defined in tools.py.
     """
     coder_state: CoderState = state.get("coder_state")
@@ -85,7 +85,14 @@ def coder_agent(state: dict) -> dict:
 graph = StateGraph(dict)
 graph.add_node("planner", planner_agent)
 graph.add_node("architect", architect_agent)
+graph.add_node("coder", coder_agent)
 graph.add_edge("planner", "architect")
+graph.add_edge("architect", "coder")
+graph.add_conditional_edges(
+    "coder",
+    lambda s: "END" if s.get("status") == "DONE" else "coder",
+    {"END": END, "coder": "coder"},
+)
 graph.set_entry_point("planner")
 
 agent = graph.compile()
@@ -93,7 +100,7 @@ agent = graph.compile()
 
 if __name__ == "__main__":
     user_prompt = "Create a simple calculator web application."
-    result = agent.invoke({"user_prompt": user_prompt})
+    result = agent.invoke({"user_prompt": user_prompt}, {"recursion_limit": 100})
     # Even though state is never explicitly defined, dict within
     # .invoke() becomes the initial global state, which LangGraph
     # automatically updates with the partial dict created by
